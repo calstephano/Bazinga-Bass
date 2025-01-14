d73ca24b7a0c71ea7c8802fa18db1d67cdfac60e
calstephano <calstephano@gmail.com>
2025-01-13 22:23:17 -0800

    Update dependency script
    - Add audio debug tools
    - Fix espeak-ng dependency and naming error

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 import asyncio
 import logging
-from src.python import wake_word_detected
+from src.python import wait_for_wake
 
 logging.basicConfig(level=logging.INFO)
 
@@ -8,8 +8,6 @@ if __name__ == "__main__":
     """Entry point for Big Mouth Bazinga Bass."""
     logging.info("Starting Big Mouth Bazinga Bass...")
     try:
-        asyncio.run(wake_word_detected())
-    except KeyboardInterrupt:
-        logging.info("Program interrupted. Shutting down gracefully...")
+        asyncio.run(wait_for_wake())
     except Exception as e:
         logging.error(f"An unexpected error occurred: {e}")
--- setup.sh
+++ setup.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+BASE_PATH=$HOME  # Adjust to the directory you cloned Bazinga-Bass in, if different
 
 # Exit immediately if a command fails
 set -e
@@ -6,10 +7,11 @@ set -e
 # Set up the project directory
 echo "Setting up the Big Mouth Bazinga Bass project..."
 
+sudo apt update
+    
 # Check for Python3
 if ! command -v python3 >/dev/null 2>&1; then
     echo "Python3 is not installed. Installing Python3..."
-    sudo apt update
     sudo apt install -y python3
 fi
 
@@ -19,19 +21,30 @@ if ! command -v pip3 >/dev/null 2>&1; then
     sudo apt install -y python3-pip
 fi
 
-# Upgrade pip to the latest version
-echo "Upgrading pip..."
-pip3 install --upgrade pip
-
 # Install system dependencies for Python packages
 echo "Installing system dependencies for Python packages..."
-sudo apt install -y python3-pip python3-dev portaudio19-dev 
+sudo apt install -y python3-dev portaudio19-dev 
 
 # Install system dependencies for C++ packages
 echo "Installing system dependencies for C++ packages..."
-sudo apt install -y pigpio
+if [ ! -d "$BASE_PATH/pigpio" ]; then
+  git clone https://github.com/joan2937/pigpio.git $HOME/pigpio
+fi
+cd $BASE_PATH/pigpio || exit
+make
+sudo make install
+sudo pigpiod
+pigs t
+sudo rm -rf
+sudo rm -rf "$BASE_PATH/pigpio"
+
+# Update and install general system dependencies (non-language-specific)
+echo "Installing system dependencies for audio and text-to-speech..."
+sudo apt update
+sudo apt install -y espeak-ng alsa-utils
 
 # Install Python packages
+cd "$BASE_PATH/Bazinga-Bass"
 if [ -f "requirements.txt" ]; then
     echo "Installing Python packages from requirements.txt..."
     pip3 install -r requirements.txt
